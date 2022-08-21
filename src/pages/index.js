7a1dde986b08785c244647d698168382e0899201
Vladimir Gareev <vladimir.R.gareev@yandex.ru>
2022-08-21 10:44:25 +0300

    доработать надпись наши принципы при горизонтальном скролле

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,19 +1,37 @@
-import { gsap } from "../../node_modules/gsap/gsap-core.js";
-import { ScrollTrigger } from "../../node_modules/gsap/ScrollTrigger.js";
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
 
 gsap.registerPlugin(ScrollTrigger);
 
+
 let sections = gsap.utils.toArray(".principles__card");
 
-gsap.to(sections, {
-	xPercent: -100*(sections.length-1),
-	ease: "none",
-	ScrollTrigger:{
-		trigger:".principles__card",
-		pin:true,
-		scrub:1,
-		snap:1/(sections.length-1),
-		end: () => "+=" +
-		document.querySelector('.principles').offsetWidth
-	}
-})
+import './index.css';
+
+// gsap.to(sections, {
+//   xPercent: -100 * (sections.length - 1),
+//   ease: "none",
+//   scrollTrigger: {
+//     trigger: ".principles",
+//     pin: true,
+//     scrub: 3,
+//     snap: 1 / (sections.length - 1),
+//     // base vertical scrolling on how wide the container is so it feels more natural.
+//     end: "+=100%",
+// 		markers: true,
+//   }
+// });
+
+gsap.to(".principles", {
+  xPercent: -66.66666,
+  ease: "none",
+  scrollTrigger: {
+    trigger: ".principles",
+    pin: true,
+    scrub: 2,
+    snap: 0.5,
+    // base vertical scrolling on how wide the container is so it feels more natural.
+    end: "+=100%",
+		markers: true,
+  }
+});
