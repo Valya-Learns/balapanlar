9aaeafda830b7adb0086bada43de8c4a668b2d49
Yury Torgashin <imaginarylight@yandex.ru>
2022-08-21 20:39:43 +0300

    сделать overflow списков, подогнать под PP

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,3 +10,17 @@ document.addEventListener("scroll", () => {
 		headerLogo.classList.add("logo_type_header-animals");
 	}
 });
+
+
+const cards = document.querySelectorAll('.course-card');
+
+cards.forEach((card) => {
+	if (card.querySelector('.course-card__list')) {
+		const courseList = card.querySelector('.course-card__list');
+		const items = courseList.querySelectorAll('.course-card__list-item');
+		if (items.length > 1) {
+			for (let i = 1; i < items.length; i++) {
+				items[i].style.display = 'none';
+	}
+		}
+}})
