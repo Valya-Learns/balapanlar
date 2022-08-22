4b955347232509d4a53c41884c730326cfd9ddf8
Yury Torgashin <imaginarylight@yandex.ru>
2022-08-22 18:05:15 +0300

    добавить компенсирующий паддинг секции Intro

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,13 +4,16 @@ import {PopupWithPartner} from '../components/PopupWithPartner.js'
 document.addEventListener("scroll", () => {
 	let scroll = window.pageYOffset;
 	const headerLogo = document.querySelector(".header").querySelector(".logo");
+	const introSection = document.querySelector(".intro");
 
 	if (scroll > 0) {
 		headerLogo.classList.remove("logo_type_header-animals");
 		headerLogo.classList.add("logo_type_header-text");
+		introSection.style.paddingTop = "48px"
 	} else {
 		headerLogo.classList.remove("logo_type_header-text");
 		headerLogo.classList.add("logo_type_header-animals");
+		introSection.style.paddingTop = "0"
 	}
 });
 const cards = document.querySelectorAll('.course-card');
