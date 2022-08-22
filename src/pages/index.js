6b9a1e38c3239737030ffcf711e8dbb08e213215
Yury Torgashin <imaginarylight@yandex.ru>
2022-08-22 18:13:19 +0300

    удалить слушатель ширины экрана

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,31 +1,24 @@
 import { PopupWithCourse } from "../components/PopupWithCourse.js";
 import {PopupWithPartner} from '../components/PopupWithPartner.js'
 
-function toggleHeader() {
 document.addEventListener("scroll", () => {
 	let scroll = window.pageYOffset;
 	const headerLogo = document.querySelector(".header").querySelector(".logo");
-		if (scroll > 0) {
-			headerLogo.classList.remove("logo_type_header-animals");
-			headerLogo.classList.add("logo_type_header-text");
-		} else {
-			headerLogo.classList.remove("logo_type_header-text");
-			headerLogo.classList.add("logo_type_header-animals");
-		}
-})}
+	const introSection = document.querySelector(".intro");
+
+	if (scroll > 0) {
+		headerLogo.classList.remove("logo_type_header-animals");
+		headerLogo.classList.add("logo_type_header-text");
+		introSection.style.paddingTop = "48px"
+	} else {
+		headerLogo.classList.remove("logo_type_header-text");
+		headerLogo.classList.add("logo_type_header-animals");
+		introSection.style.paddingTop = "0"
+	}
+});
 
 const burgerButton = document.querySelector('.burger-button');
 const burgerButtonElements = burgerButton.querySelectorAll(".burger-button__line-element");
-
-window.addEventListener("resize", () => {
-	const pageWidth = document.documentElement.scrollWidth
-	if (pageWidth > 1150) {
-		document.addEventListener("scroll", toggleHeader);
-	}
-	else {
-		document.removeEventListener("scroll", toggleHeader);
-	}
-});
 
 function enableActiveBurgerButton() {
 	element.classList.add("burger-button__line-element_active");
