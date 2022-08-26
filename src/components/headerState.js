0fb60e6bcd4e2282a850dacb3e47aaa60aa2bcc8
Yury Torgashin <imaginarylight@yandex.ru>
2022-08-26 19:32:58 +0300

    поправить код для хедера

--- src/components/headerState.js
+++ src/components/headerState.js
@@ -17,11 +17,7 @@ function setTextLogo() {
 
 function toggleHeaderState() {
 	const scroll = window.pageYOffset;
-<<<<<<< HEAD
 	if (scroll > 0) {
-=======
-	if (scroll > 1) {
->>>>>>> e6b97449999ac274327f96cd1908b811a8b98c97
 		setTextLogo();
 		firstSection.style.paddingTop = compensatePadding;
 	}
