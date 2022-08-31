2ef215a877e2b8db84d6656f0c8eff88f8247fe5
Yury Torgashin <imaginarylight@yandex.ru>
2022-08-31 20:57:27 +0300

    подогнать попап на десктопе под PP

--- src/components/PopupWithCourse.js
+++ src/components/PopupWithCourse.js
@@ -17,8 +17,8 @@ export class PopupWithCourse extends Popup {
 		const btn = content.querySelector('.course-card__popup-button');
 		btn.textContent = 'Закрыть';
 		btn.addEventListener('mousedown', () => super.close())
-		content.querySelector('.flex-container_info').style = 'flex-direction: row';
-		content.querySelector('.flex-container_text').style = 'gap: 30px';
+		content.querySelector('.flex-container_type_info').style = 'flex-direction: row';
+		content.querySelector('.flex-container_type_text').style = 'gap: 30px';
 		content.querySelector('.course-card__list').className = 'popup__text-skills';
 		content.querySelectorAll('.course-card__list-item').forEach(item => {
 			item.classList.remove('course-card__list-item')
